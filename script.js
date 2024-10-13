d26a409236c48d23abc9a7fb1af5981580e06984
aarshpatel1 <aarsh18patel@gmail.com>
2024-10-13 23:08:38 +0530

    give default values 0 and date and set a media breakpoint for container for large screen

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const country = document.getElementById("country")
 const search = document.getElementById("search")
 
+document.getElementById("last-report-date").textContent = todaysDate()
 
 search.addEventListener("click", function () {
     var countryName = country.value.trim()
@@ -16,20 +17,19 @@ search.addEventListener("click", function () {
         }
     };
 
-    fetch(url, options).then((response) => response.json()).then((x) => {
-        console.log(x)
-        document.getElementById("population").textContent = printValue(x.response[0].population)
-        document.getElementById("new-cases").textContent = printValue(x.response[0].cases.new)
-        document.getElementById("active-cases").textContent = printValue(x.response[0].cases.active)
-        document.getElementById("recovered").textContent = printValue(x.response[0].cases.recovered)
-        document.getElementById("cases-per-million").textContent = printValue(x.response[0].cases["1M_pop"])
-        document.getElementById("total-cases").textContent = printValue(x.response[0].cases.total)
-        document.getElementById("new-deaths").textContent = printValue(x.response[0].deaths.new)
-        document.getElementById("deaths-per-million").textContent = printValue(x.response[0].deaths["1M_pop"])
-        document.getElementById("total-deaths").textContent = printValue(x.response[0].deaths.total)
-        document.getElementById("test-per-million").textContent = printValue(x.response[0].tests["1M_pop"])
-        document.getElementById("total-test").textContent = printValue(x.response[0].tests.total)
-        document.getElementById("last-report-date").textContent = printValue(x.response[0].day)
+    fetch(url, options).then((response) => response.json()).then((data) => {
+        document.getElementById("population").textContent = printValue(data.response[0].population)
+        document.getElementById("new-cases").textContent = printValue(data.response[0].cases.new)
+        document.getElementById("active-cases").textContent = printValue(data.response[0].cases.active)
+        document.getElementById("recovered").textContent = printValue(data.response[0].cases.recovered)
+        document.getElementById("cases-per-million").textContent = printValue(data.response[0].cases["1M_pop"])
+        document.getElementById("total-cases").textContent = printValue(data.response[0].cases.total)
+        document.getElementById("new-deaths").textContent = printValue(data.response[0].deaths.new)
+        document.getElementById("deaths-per-million").textContent = printValue(data.response[0].deaths["1M_pop"])
+        document.getElementById("total-deaths").textContent = printValue(data.response[0].deaths.total)
+        document.getElementById("test-per-million").textContent = printValue(data.response[0].tests["1M_pop"])
+        document.getElementById("total-test").textContent = printValue(data.response[0].tests.total)
+        document.getElementById("last-report-date").textContent = printValue(data.response[0].day)
     }).catch((err) => console.log(err))
 })
 
@@ -39,4 +39,14 @@ function printValue(data) {
         return 0
     else
         return data
+}
+
+function todaysDate() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0'); // JavaScript months are 0-indexed
+    const day = String(today.getDate()).padStart(2, '0');
+
+    const formattedDate = `${year}-${month}-${day}`;
+    return formattedDate
 }
