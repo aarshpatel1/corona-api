77e85701e886aea3cf072238095c502c5f75febb
aarshpatel1 <aarsh18patel@gmail.com>
2024-10-13 23:01:08 +0530

    api ui is ready

--- script.js
+++ script.js
@@ -18,18 +18,18 @@ search.addEventListener("click", function () {
 
     fetch(url, options).then((response) => response.json()).then((x) => {
         console.log(x)
-        document.getElementById("population").innerHTML = `Pop${printValue(x.response[0].population)}`
-        document.getElementById("new-cases").innerHTML = `${printValue(x.response[0].cases.new)}`
-        document.getElementById("active-cases").innerHTML = `${printValue(x.response[0].cases.active)}`
-        document.getElementById("recovered").innerHTML = `${printValue(x.response[0].cases.recovered)}`
-        document.getElementById("cases-per-million").innerHTML = `${printValue(x.response[0].cases["1M_pop"])}`
-        document.getElementById("total-cases").innerHTML = `${printValue(x.response[0].cases.total)}`
-        document.getElementById("new-deaths").innerHTML = `${printValue(x.response[0].deaths.new)}`
-        document.getElementById("deaths-per-million").innerHTML = `${printValue(x.response[0].deaths["1M_pop"])}`
-        document.getElementById("total-deaths").innerHTML = `${printValue(x.response[0].deaths.total)}`
-        document.getElementById("test-per-million").innerHTML = `${printValue(x.response[0].tests["1M_pop"])}`
-        document.getElementById("total-test").innerHTML = `${printValue(x.response[0].tests.total)}`
-        document.getElementById("last-report-date").innerHTML = `${printValue(x.response[0].day)}`
+        document.getElementById("population").textContent = printValue(x.response[0].population)
+        document.getElementById("new-cases").textContent = printValue(x.response[0].cases.new)
+        document.getElementById("active-cases").textContent = printValue(x.response[0].cases.active)
+        document.getElementById("recovered").textContent = printValue(x.response[0].cases.recovered)
+        document.getElementById("cases-per-million").textContent = printValue(x.response[0].cases["1M_pop"])
+        document.getElementById("total-cases").textContent = printValue(x.response[0].cases.total)
+        document.getElementById("new-deaths").textContent = printValue(x.response[0].deaths.new)
+        document.getElementById("deaths-per-million").textContent = printValue(x.response[0].deaths["1M_pop"])
+        document.getElementById("total-deaths").textContent = printValue(x.response[0].deaths.total)
+        document.getElementById("test-per-million").textContent = printValue(x.response[0].tests["1M_pop"])
+        document.getElementById("total-test").textContent = printValue(x.response[0].tests.total)
+        document.getElementById("last-report-date").textContent = printValue(x.response[0].day)
     }).catch((err) => console.log(err))
 })
 
